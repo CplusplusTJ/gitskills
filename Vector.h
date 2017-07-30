a6469219b96e8e75966b8c1dc16b7f68998bf854
田杰 <CplusplusTJ>
2017-07-30 09:58:39 +0800

    write Vector.h

---
+++ Vector.h
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<alrorithm>
+
+template<typename Object>
+class Vector{
+
+public:
+	//拷贝控制流程6个
+	explicit Vector(int);
+	Vector(const Vector&);
+	Vector &operator=(const Vector&);
+	~Vector(){delete object;}
+	Vector(Vector&&);
+	Vector &operator=(Vector&&);
+	
+	//管理内存2个
+	void resize(int );
+	void reserve(int );
+
+	//功能函数9
+	Object operator[](int){}
+	const Object operator[](int)const{}
+	bool empty()const{}
+	int size()const{}
+	int capacity()const{}
+	void push_back(const Object&);
+	void push_back(Object&&);
+	void pop();
+	const Object &back()const{}	
+	
+	//使用迭代器的函数
+	typedef Object *iterator;
+	typedef const Object const_iterator;
+	
+	iterator begin(){}
+	const_iterator begin()const{}
+	iterator end(){}
+	const_iterator end()const{}
+
+	static const int SPARE_CAPACITY=16;
+	
+	
+	
+	
+private:
+	int theSize;
+	int theCapacity;
+	Object *object;
+	
+};
